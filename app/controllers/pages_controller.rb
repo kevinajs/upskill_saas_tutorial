4de86e2f499786c058c4bdc8de86ccbdd4512869
Kevin Saunders <kevinajsaunders@gmail.com>
2017-01-09 01:22:07 +0000

    Added plan id as hidden fields to signup form.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  # GET request for / which is out home page
+  # GET request for / which is our home page
   def home
     @basic_plan = Plan.find(1)
     @pro_plan = Plan.find(2)
